4c8ec698ec8594fd2a8af1826b2d2fd837c6fbe7
David Thorn <david.thorn221278@googlemail.com>
2018-11-23 21:52:46 +0100

    introduced const and variables and the number type assignment...

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,71 @@ window.onload = () => {
     }
 
     const container = document.getElementById('container') as HTMLElement
-    const header = document.createElement('h1')
-    let text  = document.createTextNode(`${youtube.name} ${youtube.surname} user is watching this video`)
-    header.appendChild(text)
-    container.appendChild(header)
-    console.log('loaded', youtube)
+    // const header = document.createElement('h1')
+    // let text  = document.createTextNode(`${youtube.name} ${youtube.surname} user is watching this video`)
+    // header.appendChild(text)
+    // container.appendChild(header)
+  
+
+    /// number
+    /// double
+    /// float
+    /// array
+    /// null
+    /// object
+    /// undefined
+    /// string
+    /// []
+    /// {}
+    /// object
+    /// enum
+    /// any
+
+    const davidsAge: number = 39
+    const num1: number = 2
+    const sum: number = davidsAge + num1
+
+    console.log(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
